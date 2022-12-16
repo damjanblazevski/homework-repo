5892cd2518489ba1f4117f3afb5f6b2558e2419c
damjanblazevski <119970932+damjanblazevski@users.noreply.github.com>
2022-12-16 17:30:05 +0100

    Finished Homework

---
+++ Homework with Functions (Seavus JS)/script/script.js
@@ -0,0 +1,10 @@
+function printType(param) {
+    console.log(`Type of ${param}: ${typeof param}`);
+  }
+  
+  printType({});
+  printType(true);
+  printType(123);
+  printType('hello');
+  printType(undefined);
+  
