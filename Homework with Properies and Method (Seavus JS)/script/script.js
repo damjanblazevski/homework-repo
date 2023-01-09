83691fb799fd0ad0560afbbb3e9eb72b1454b024
damjanblazevski <119970932+damjanblazevski@users.noreply.github.com>
2023-01-09 15:31:24 +0100

    Finished Homework 4
    Sekoja domashna si ima svoj folder, toest samo folderot go imenuvam kako naslovot na domashnata i taka go prakjam.

---
+++ Homework with Properies and Method (Seavus JS)/script/script.js
@@ -0,0 +1,37 @@
+console.log("It works!");
+
+/*
+const animal = {
+    name: "",
+    kind: "",
+    speak: "Dog Says: Hey Bro!!!",
+
+    animalFunction() {
+        console.log(`${this.speak}`);
+    },
+};
+
+animal.animalFunction();
+*/
+
+const bookOne = {
+    titleOne: "The Robots of dawn",
+    authorOne: "Isaac Asimov",
+    readingStatusOne: true,
+}
+console.log(bookOne);
+
+const bookTwo = {
+    titleTwo: "Mockingjay: The Final Book of The Hunger Games",
+    authorTwo: "Suzanne Collins",
+    readingStatusTwo: false
+}
+console.log(bookTwo);
+
+bookOneFunction() {
+    console.log(`Already red '${titleOne}' by ${authorOne}`);
+};
+
+// Do ovde stignav celo vreme greska mi dava na "bookOneFunction" probav sto nacini dzabe ne saka :/
+
+// Gornata zadaca so dog samo trgni gi komentarite i taa gotovo e mislam
